57a5cdbbd1e52e9ebae69fef3d4e7ef1702fa534
kavindu <kavindupath@gmail.com>
2020-10-07 16:38:12 +0530

    Project Directory walkthrough

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+//import libraries for making a component
+import React from 'react';
+import {Text} from 'react-native';
+
+//making a component
+const Header = () => {
+
+    return <Text>Albums!</Text>;
+
+};
+
+//make the component available to other parts of the app
+export default Header;
