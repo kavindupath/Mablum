2d561183592be674ce1df3cdf54ff6feb30395cb
kavindu <kavindupath@gmail.com>
2020-10-08 20:31:50 +0530

    Styling the Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,41 @@
 /* eslint-disable prettier/prettier */
 //import libraries for making a component
 import React from 'react';
-import {Text} from 'react-native';
+import { Text, View } from 'react-native';
 
 //making a component
 const Header = () => {
 
-    const {textStyle} = styles;
-    return <Text style={textStyle}>Albums!</Text>;
+    const { textStyle, viewStyle } = styles;
 
+    return (
+        <View style={viewStyle}>
+
+            <Text style={textStyle}>Albums!!!!!</Text>
+        </View>
+    );
 };
 
+
+
+
 const styles = {
-    textStyle:{
-        fontSize:20,
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent:'center',
+        alignItems:'center',
+        height:60,
+        paddingTop:15,
+        shadowColor:'#000',
+        shadowOffset: {width:0,height:2},
+        shadowOpacity:0.2,
+        elevation:2,
+        position: 'relative'
+
+
+    },
+    textStyle: {
+        fontSize: 20,
     },
 };
 
